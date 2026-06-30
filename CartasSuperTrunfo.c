62faec277b8fea1ec8ee5e0a065bf92755ebc626
Reinhard Oliveira <oliveiralimaeliabe14@gmail.com>
2026-06-30 23:10:10 +0000

    Fiz o desafio Super Trunfo em C, nível novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,102 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+
   // Área para definição das variáveis para armazenar as propriedades das cidades
+
+ char estado1 [50];
+
+    char codigo1 [50];
+
+    char nomecidade1 [50];
+
+    int populacao1;
+
+    float area1;
+
+    float pib1;
+
+    int pontosturisticos1;
+
+   char estado2 [50];
+
+    char codigo2 [50];
+
+    char nomecidade2 [50];
+
+    int populacao2;
+
+    float area2;
+
+    float pib2;
+
+    int pontosturisticos2;
 
   // Área para entrada de dados
 
+printf("Nome do Estado: \n");
+    scanf(" %[^\n]", estado1);
+
+  printf("Código da Carta: \n");
+    scanf(" %s", codigo1);
+    
+  printf("Nome da Cidade: \n");
+    scanf(" %[^\n]", nomecidade1);
+
+  printf("População: \n");
+    scanf(" %d", &populacao1);
+    
+  printf("Área: \n");
+    scanf(" %f", &area1);
+    
+  printf("PIB: \n");
+    scanf(" %f", &pib1);
+
+  printf("Pontos Turísticos: \n");
+    scanf(" %d", &pontosturisticos1);
+
+  printf("Nome do Estado: \n");
+    scanf(" %[^\n]", estado2);
+
+  printf("Código da Carta: \n");
+    scanf(" %s", codigo2);
+    
+  printf("Nome da Cidade: \n");
+    scanf(" %[^\n]", nomecidade2);
+
+  printf("População: \n");
+    scanf(" %d", &populacao2);
+    
+  printf("Área: \n");
+    scanf(" %f", &area2);
+    
+  printf("PIB: \n");
+    scanf(" %f", &pib2);
+
+  printf("Pontos Turísticos: \n");
+    scanf(" %d", &pontosturisticos2);
+
   // Área para exibição dos dados da cidade
+printf("\nCarta 1 \n");
+printf("Estado: %s \n", estado1);
+printf("Código: %s \n", codigo1);
+printf("Nome da Cidade: %s \n", nomecidade1);
+printf("População: %d \n", populacao1);
+printf("Área: %.2f \n", area1);
+printf("PIB: %.2f \n", pib1);
+printf("Número de Pontos Turísticos: %d \n", pontosturisticos1);
+
+printf("Carta 2 \n");
+printf("Estado: %s \n", estado2);
+printf("Código: %s \n", codigo2);
+printf("Nome da Cidade: %s \n", nomecidade2);
+printf("População: %d \n", populacao2);
+printf("Área: %.2f \n", area2);
+printf("PIB: %.2f \n", pib2);
+printf("Número de Pontos Turísticos: %d \n", pontosturisticos2);
+
+printf("\nPressione Enter para Terminar...");
+getchar();
 
 return 0;
 } 
